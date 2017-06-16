7894b10b02c401232f4a91954a2bf8e14d0d17de
Georgiy Frolov <gfrolov@sensics.com>
2017-06-16 12:08:22 -0400

    add IVRDriverManager implementation

---
+++ DriverManager.cpp
@@ -0,0 +1,53 @@
+/** @file
+    @brief Implementation
+
+    @date 2017
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2017 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include "DriverManager.h"
+#include "ValveStrCpy.h"
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+using namespace vr;
+
+DriverManager::DriverManager()
+    : m_logger(osvr::util::log::make_logger("DriverManager")) {}
+
+uint32_t DriverManager::GetDriverCount() const {
+    m_logger->info("GetDriverCount");
+    return 1;
+}
+uint32_t DriverManager::GetDriverName(vr::DriverId_t nDriver, char *pchValue,
+                                      uint32_t unBufferSize) {
+    m_logger->info("GetDriverName(") << nDriver << "," << unBufferSize << ")";
+    const std::string driverName = "lighthouse";
+    if (unBufferSize > driverName.size()) {
+        auto ret = valveStrCpy(driverName, pchValue, driverName.size());
+        return ret;
+    }
+    return 0;
+}
---
+++ DriverManager.h
@@ -0,0 +1,51 @@
+/** @file
+    @brief Header
+
+    @date 2017
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2017 Sensics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INCLUDED_DriverManager_h_GUID_BC08BDF5_CBD0_4DAF_F30F_43DE8357A43B
+#define INCLUDED_DriverManager_h_GUID_BC08BDF5_CBD0_4DAF_F30F_43DE8357A43B
+
+// Internal Includes
+#include <osvr/Util/Logger.h>
+
+// Library/third-party includes
+#include <openvr_driver.h>
+
+// Standard includes
+#include <cstdint>
+
+namespace vr {
+
+class DriverManager : public vr::IVRDriverManager {
+  public:
+    DriverManager();
+    virtual uint32_t GetDriverCount() const;
+    virtual uint32_t GetDriverName(vr::DriverId_t nDriver, char *pchValue,
+                                   uint32_t unBufferSize);
+  private:
+    osvr::util::log::LoggerPtr m_logger;
+};
+
+} // namespace vr
+
+#endif // INCLUDED_DriverManager_h_GUID_BC08BDF5_CBD0_4DAF_F30F_43DE8357A43B
